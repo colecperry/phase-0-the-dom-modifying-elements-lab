ea35471fc76ee809bb7d944680bacb047d5b8650
Cole Perry <colecperry@gmail.com>
2023-02-17 14:57:20 -0800

    Done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const element = document.querySelector("main");
+element.remove("main")
+
+const newHeader = document.createElement("h1");
+newHeader.id = "victory"
+newHeader.textContent = "Cole is the champion"
+
+document.body.append(newHeader)
